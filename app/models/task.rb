725878a2ac7ea92644876db83d46000027c7023d
Wontae Yang <wontaeyang@gmail.com>
2013-02-02 17:58:18 -0500

    stable menu

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,8 @@ class Task < ActiveRecord::Base
   belongs_to :project
 
   validates :project_id, presence: true
-  validates :description, presence: true
+  validates :description, presence: true, uniqueness: true
+
 
   default_scope order: 'tasks.created_at DESC'
 end
